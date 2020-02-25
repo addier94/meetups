591d11142223ae65666f5371f0bb372684d0427d
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-02-25 17:44:36 -0400

    Part 11 Introduction to Vues Actions

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -41,22 +41,17 @@
       CategoryItem,
       MeetupItem
     },
-    data() {
-      return {
-        meetups: [],
-        categories: []
+    computed: {
+      meetups () {
+        return this.$store.getters['meetups']
+      },
+      categories () {
+        return this.$store.getters['categories']
       }
     },
     created () {
-      axios.get('/api/v1/meetups')
-        .then(res => {
-          this.meetups = res.data
-        })
-
-      axios.get('/api/v1/categories')
-        .then(res => {
-          this.categories = res.data
-        })
+      this.$store.dispatch('fetchMeetups')
+      this.$store.dispatch('fetchCategories')
     }
   }
 </script>
